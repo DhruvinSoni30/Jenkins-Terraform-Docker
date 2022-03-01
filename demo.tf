7f66bf7f721e1a6b726a226ac8d572cfcf190275
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-03-01 11:36:06 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -191,7 +191,7 @@ resource "aws_instance" "demoinstance1" {
       "sudo yum install -y git",
       "sudo chmod 666 /var/run/docker.sock",
       "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"   
+      "docker run -d -p 80:80 dhruvin30/dhsoniweb:v1"   
   ]
  }
 }
